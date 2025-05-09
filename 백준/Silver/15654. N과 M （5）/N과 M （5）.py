0d716ed74b64e5fbb8aec6fec5161f7a5500ff72
이총명 <dlchdaud04@naver.com>
2025-05-09 22:34:04 +0900

    [Silver III] Title: N과 M (5), Time: 192 ms, Memory: 118456 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -0,0 +1,21 @@
+def dp(temp):
+    global visited
+    if len(temp)==m:
+        visited.append(temp[:])
+        return
+    else:
+        for i in li:
+            if i not in temp:
+                temp.append(i)
+                dp(temp)
+                temp.pop()
+
+
+n,m=map(int,input().split())
+li = list(map(int,input().split()))
+li.sort()
+visited=[]
+
+dp([])
+for i in visited:
+    print(*i)
