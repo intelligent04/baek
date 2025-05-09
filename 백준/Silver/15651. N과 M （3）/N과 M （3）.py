768410af7549138f8466c1dac744afff8b23a6df
이총명 <dlchdaud04@naver.com>
2025-05-09 22:15:41 +0900

    [Silver III] Title: N과 M (3), Time: 2712 ms, Memory: 144400 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,16 @@
+def dp(li):
+    global visited
+    if(len(li)==m):
+        visited.append(li[:])
+    else:
+        for i in range(1,n+1):
+            li.append(i)
+            dp(li)
+            li.pop()
+
+n,m = map(int,input().split())
+li=[]
+visited=[]
+dp(li)
+for i in visited:
+    print(*i)
